e38dd1153af8962abb6962535210941925161824
derroh <derrickwitness@gmail.com>
2021-10-16 02:00:26 +0300

    Added list of requests

--- Areas/ADMIN/Controllers/RequestsController.cs
+++ Areas/ADMIN/Controllers/RequestsController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Murugi_25July.Models;
+using Murugi_25July.ViewModels;
+using Newtonsoft.Json;
 
 namespace Murugi_25July.Areas.ADMIN.Controllers
 {
@@ -11,7 +14,38 @@ namespace Murugi_25July.Areas.ADMIN.Controllers
         // GET: ADMIN/Requests
         public ActionResult Index()
         {
-            return View();
+            List<RequestsListViewModel> _UsersViewModel = new List<RequestsListViewModel>();
+            //fetch from database
+            try
+            {
+                using (var db = new EmergencyMedicalAndAmbulanceDispatchEntities())
+                {
+                    var list = db.Requests.ToList();
+
+                    foreach (var request in list)
+                    {
+                        _UsersViewModel.Add(new RequestsListViewModel 
+                        {
+                            Amount = request.Amount,
+                            Date_Time = request.Date_Time.ToString(),
+                            Description = request.Description,
+                            Location = request.Location,
+                            RequesterIdNumber = request.RequesterIdNumber,
+                            RequestID = request.RequestID,
+                            RequestStatus = request.RequestStatus,
+                            RequestType = request.RequestType,
+                            VehicleRegistrationNo = request.VehicleRegistrationNo
+                        });
+                    }
+                }
+
+            }
+            catch (Exception es)
+            {
+                string x = es.InnerException.Message;
+                x = "" + x;
+            }
+            return View(_UsersViewModel);
         }
     }
 }
---
+++ ViewModels/RequestsListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Murugi_25July.ViewModels
+{
+    public class RequestsListViewModel
+    {
+        public string RequestID { get; set; }
+        public string RequestType { get; set; }
+        public string Description { get; set; }
+        public string Date_Time { get; set; }
+        public string RequesterIdNumber { get; set; }
+        public int Location { get; set; }
+        public string VehicleRegistrationNo { get; set; }
+        public decimal Amount { get; set; }
+        public string RequestStatus { get; set; }
+    }
+}
